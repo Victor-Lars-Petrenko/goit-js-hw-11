5807e9c206d4313b8ba000e074c59aab8d997386
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2023-12-01 20:07:03 +0200

    done-4

---
+++ src/create-markup.js
@@ -0,0 +1,23 @@
+export default function createMarkup(arr) {
+    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+        `<div class="photo-card">
+            <a href="${largeImageURL}" class="img-link">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b> ${likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b> ${views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b> ${comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b> ${downloads}
+                </p>
+            </div>
+        </div>`
+    ).join("");
+}
--- src/index.js
+++ src/index.js
@@ -1,65 +1,33 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from "axios";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import getImages from "./api";
+import createMarkup from "./create-markup";
 
 const refs = {
     form: document.querySelector(".search-form"),
     loadMoreBtn: document.querySelector(".load-more"),
-    gallery: document.querySelector(".gallery")
-}
-
-async function getImages(searchQuery, pageNumber) {
-    const searchParams = new URLSearchParams({
-        key: "40998615-e942d78fa7d7f729cbc4e5126",
-        q: searchQuery,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true,
-        page: pageNumber,
-        per_page: 40
-    });
-    const URL = `https://pixabay.com/api/?${searchParams}`
-
-    const fetchResult = await axios.get(URL);
-    const { data } = fetchResult;
-
-    return data;
-}
-
-function createMarkup(arr) {
-    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-        `<div class="photo-card">
-            <a href="${largeImageURL}" class="img-link">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            </a>
-            <div class="info">
-                <p class="info-item">
-                    <b>Likes</b> ${likes}
-                </p>
-                <p class="info-item">
-                    <b>Views</b> ${views}
-                </p>
-                <p class="info-item">
-                    <b>Comments</b> ${comments}
-                </p>
-                <p class="info-item">
-                    <b>Downloads</b> ${downloads}
-                </p>
-            </div>
-        </div>`
-    ).join("");
+    gallery: document.querySelector(".gallery"),
+    submitBtn: document.querySelector(".submit-btn")
 }
 
 let pageNumber = 1;
-const lightbox = new SimpleLightbox('.gallery a')
+let userInput;
+const lightbox = new SimpleLightbox('.gallery a');
 
 refs.form.addEventListener("submit", e => {
     e.preventDefault();
     refs.loadMoreBtn.classList.add("visually-hidden");
 
-    const userInput = refs.form.elements.searchQuery.value;
+    userInput = refs.form.elements.searchQuery.value.trim();
     pageNumber = 1;
+
+    if (userInput === "") {
+        Notify.failure("Please fill input field");
+        return
+    }
+
+    refs.submitBtn.disabled = true;
 
     getImages(userInput, pageNumber).then(({ totalHits, hits }) => {
         if (totalHits === 0) {
@@ -69,33 +37,41 @@ refs.form.addEventListener("submit", e => {
 
         refs.gallery.innerHTML = createMarkup(hits);
 
-        lightbox.refresh()
-        if (totalHits > 40) {
-            refs.loadMoreBtn.classList.remove("visually-hidden");
+        lightbox.refresh();
+        refs.submitBtn.disabled = false;
+
+        if (totalHits <= 40) {
+            Notify.failure("We're sorry, but you've reached the end of search results.");
+            return
         }
+
+        refs.loadMoreBtn.classList.remove("visually-hidden");
     }).catch(() => {
         refs.gallery.innerHTML = "";
         refs.loadMoreBtn.classList.add("visually-hidden");
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        refs.submitBtn.disabled = false;
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     })
 })
 
-refs.loadMoreBtn.addEventListener("click", e => {
-    const userInput = refs.form.elements.searchQuery.value;
+refs.loadMoreBtn.addEventListener("click", () => {
+    refs.loadMoreBtn.classList.add("visually-hidden");
 
     pageNumber += 1;
 
     getImages(userInput, pageNumber).then(({ totalHits, hits }) => {
-        if (40 * (pageNumber - 1) > totalHits) {
-            throw new Error
-        }
-
         const markup = createMarkup(hits);
         refs.gallery.insertAdjacentHTML("beforeend", markup);
 
-        lightbox.refresh()
+        lightbox.refresh();
+        refs.loadMoreBtn.disabled = false;
+
+        if (pageNumber >= Math.ceil(totalHits / 40)) {
+            throw new Error;
+        }
+
+        refs.loadMoreBtn.classList.remove("visually-hidden");
     }).catch(() => {
-        refs.loadMoreBtn.classList.add("visually-hidden");
-        Notify.failure("We're sorry, but you've reached the end of search results.")
+        Notify.failure("We're sorry, but you've reached the end of search results.");
     })
 })
